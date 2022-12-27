68919829496b164809bfd4b48b505672d9978e42
Sammy Taylor <sam.taylor@me.com>
2022-12-27 01:45:04 -0600

    Next and third characters in lexer

--- src/Lexer/Lexer.cpp
+++ src/Lexer/Lexer.cpp
@@ -4,19 +4,30 @@
 #pragma once
 #include "../CharacterType.cpp"
 #include "../charTypeFrom.cpp"
+#include "../utils.cpp"
 
 void Lexer::lex(std::string input) {
-  std::cout << "Lexer received input." << std::endl << input << std::endl;
+  PRINT("Lexer received input:" << std::endl << input);
 
   // TODO: We will not have UTF8 support until we change this. "input.length" is just bytes.
   int max = input.length();
   for (size_t index = 0; index < max; index++) {
     char character = input[index];
-    char nextCharacter = input[index + 1];
-    char thirdCharacter = input[index + 1];
+    char nextCharacter;
+    char thirdCharacter;
     CharacterType charType = charTypeFrom(character);
+    CharacterType nextCharacterType;
+    CharacterType thirdCharacterType;
+    if (index < max - 1) {
+      nextCharacter = input[index + 1];
+      nextCharacterType = charTypeFrom(nextCharacter);
+    }
+    if (index < max - 2) {
+      thirdCharacter = input[index + 2];
+      thirdCharacterType = charTypeFrom(thirdCharacter);
+    }
 
-    std::cout << "======================" << std::endl;
-    std::cout << "Character: " << character << " " << charType << std::endl;
+    PRINT("======================");
+    PRINT("Character: " << character << " " << charType);
   }
 };
