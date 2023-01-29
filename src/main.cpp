4a956dd371377e628cb6d79b5312cb82ce2f2bf8
Sammy Taylor <sam.taylor@me.com>
2023-01-28 23:51:02 -0600

    Demonstrate JSON lib usage
    Will revert this commit—just here as proof of concept. I'm really, really new to c++.

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,25 @@
 #include "./InterpolationContextStack/InterpolationContextStack.cpp"
 #include "./Lexer/Lexer.cpp"
 #include <iostream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 std::string testInput = R"sammy(
 a = 1 + 2
 )sammy";
 
 int main() {
+  // Print some "hello world" JSON to the console to prove it works.
+  json ex1 = json::parse(R"(
+  {
+    "pi": 3.141,
+    "happy": true
+  }
+)");
+
+  std::cout << ex1;
+
   print("Compiling...");
   Lexer lexer = Lexer();
   lexer.lex(testInput);
