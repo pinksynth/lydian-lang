a44547fc976e7d54e566cb0b2b02ac525a80a1f0
Sammy Taylor <sam.taylor@me.com>
2023-01-30 23:02:04 -0600

    Unit test for Lambdas

---
+++ tests/cases/lambdas-1/test.cpp
@@ -0,0 +1,4 @@
+TEST_CASE("Lambdas 1") {
+  print("Testing: Lambdas 1");
+  doLexerAssertions(__FILE__);
+}
--- tests/test.cpp
+++ tests/test.cpp
@@ -18,4 +18,5 @@
 #include "./cases/booleans-1/test.cpp"
 #include "./cases/comments-1/test.cpp"
 #include "./cases/function-definitions-1/test.cpp"
+#include "./cases/lambdas-1/test.cpp"
 #include "./cases/small-file-1/test.cpp"
