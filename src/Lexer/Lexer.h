65d5a30ae96cf7f904ddc6bbc12edd4882e2d60c
Sammy Taylor <sam.taylor@me.com>
2023-02-05 16:39:14 -0600

    POC: LLVM Usage
    Will revert this commit, just demonstrating usage

--- src/Lexer/Lexer.h
+++ src/Lexer/Lexer.h
@@ -2,11 +2,26 @@
 #include "../InterpolationContextStack/InterpolationContextStack.h"
 #include "../Token.cpp"
 #include "../TokenType.cpp"
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
 #include <iostream>
 #include <vector>
 
 #pragma once
+
 class Lexer {
+
+  static std::unique_ptr<llvm::LLVMContext> TheContext;
+
   // These values change while in the loop to consume characters.
   int index;
   char previousCharacter;
