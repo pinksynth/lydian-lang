0b813cf3009f42647daff65150489d5c48cfbf3a
Sammy Taylor <sam.taylor@me.com>
2023-05-30 22:05:40 -0500

    Refactor doAssertions into interior functions

--- tests/testUtils.cpp
+++ tests/testUtils.cpp
@@ -1,4 +1,45 @@
-#include "dtl/dtl.hpp"
+#include <array>
+#include <cstdio>
+#include <cstring>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <fmt/core.h>
+
+// Gets output from an arbitrary system command.
+//   https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
+std::string getOutputFromSystemCommand(std::string strCmd) {
+  const char *cmd = strCmd.c_str();
+  std::array<char, 128> buffer;
+  std::string result;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+
+  if (!pipe)
+    throw std::runtime_error("popen() failed!");
+
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    result += buffer.data();
+
+  return result;
+}
+
+std::string writeStringToTmpFile(std::string contents) {
+  char tmpFileName[PATH_MAX];
+  snprintf(tmpFileName, PATH_MAX - 1, "./%s.XXXXXX", "tmp");
+  int fileDescriptor = mkstemp(tmpFileName);
+  FILE *outFile = fdopen(fileDescriptor, "w");
+  const char *contentsCStr = contents.c_str();
+  write(fileDescriptor, contentsCStr, strlen(contentsCStr));
+  fclose(outFile);
+  std::string stringFileName(tmpFileName);
+  return stringFileName;
+}
 
 std::vector<Token> tokenArrayFromJson(json jsonArray) {
   std::vector<Token> tokens = {};
@@ -35,8 +76,7 @@ void compareTokens(std::vector<Token> expectedTokens, std::vector<Token> receive
   }
 }
 
-// Makes assertions based on three files per test case. The test case's `input.sammy` is the Sammy Lang input. The Lexer produces tokens and those tokens are compared with `expectedTokens.json`. Then the tokens are fed to the AST, which is expected to look like `expectedAST.json`.
-void doAssertions(std::filesystem::path currentFile) {
+std::vector<Token> doTokenAssertions(std::filesystem::path currentFile) {
   std::ifstream inputFileStream(currentFile.parent_path() / "input.sammy");
   std::ostringstream inputFileStreamString;
   inputFileStreamString << inputFileStream.rdbuf();
@@ -51,17 +91,45 @@ void doAssertions(std::filesystem::path currentFile) {
 
   compareTokens(expectedTokens, receivedTokens);
 
+  return receivedTokens;
+}
+
+void doASTAssertions(std::filesystem::path currentFile, std::vector<Token> receivedTokens) {
   std::filesystem::path expectedASTPath(currentFile.parent_path() / "expectedAST.json");
 
   if (!std::filesystem::exists(expectedASTPath)) {
     alert("WARNING: Could not find " + expectedASTPath.generic_string());
   } else {
+
     std::ifstream expectedASTFileStream(expectedASTPath);
-    json expectedASTJson = json::parse(expectedASTFileStream);
+    std::string expectedASTJson = json::parse(expectedASTFileStream).dump(2);
 
     SammyAST ast = SammyAST();
     ast.fromTokens(receivedTokens);
-    json receivedASTJson = ast.jsonAST;
-    REQUIRE(expectedASTJson == receivedASTJson);
+    std::string receivedASTJson = ast.jsonAST.dump(2);
+
+    if (expectedASTJson != receivedASTJson) {
+      std::string tmpFilename1 = writeStringToTmpFile(expectedASTJson);
+      std::string tmpFilename2 = writeStringToTmpFile(receivedASTJson);
+
+      char diffOutputFilename[PATH_MAX];
+      snprintf(diffOutputFilename, PATH_MAX - 1, "./%s.XXXXXX", "tmp");
+      int diffOutputFd = mkstemp(diffOutputFilename);
+      FILE *diffOut = fdopen(diffOutputFd, "w");
+
+      alert("AST is invalid for\n\n  " + currentFile.generic_string());
+      std::cout << getOutputFromSystemCommand(
+                       fmt::format("diff -u --color=always {} {}", tmpFilename1, tmpFilename2))
+                << std::endl;
+      fclose(diffOut);
+      remove(tmpFilename1.c_str());
+      remove(tmpFilename2.c_str());
+    };
   }
+}
+
+// Makes assertions based on three files per test case. The test case's `input.sammy` is the Sammy Lang input. The Lexer produces tokens and those tokens are compared with `expectedTokens.json`. Then the tokens are fed to the AST, which is expected to look like `expectedAST.json`.
+void doAssertions(std::filesystem::path currentFile) {
+  std::vector<Token> receivedTokens = doTokenAssertions(currentFile);
+  doASTAssertions(currentFile, receivedTokens);
 }
